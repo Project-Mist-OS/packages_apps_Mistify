ProjectEverest/packages_apps_BaseCamp

Theme Room: Introduce bootanimation styles [2/3]
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

----

Themes: Move bootanimation settings to its fragment

 * Also add bootanimation preview and custom bootanimation import

Co-authored-by: Alvin Francis <nivlafx@gmail.com>

src/com/everest/basecamp/preferences/BootAnimationPreviewPreference.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.preferences;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.R;
+import com.everest.basecamp.utils.BootAnimationUtils;
+
+public class BootAnimationPreviewPreference extends Preference {
+
+    private ImageView mImageView;
+    private ProgressBar mLoadingSpinner;
+    private LoadPreviewTask mCurrentTask;
+
+    public BootAnimationPreviewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_bootanimation_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mImageView = (ImageView) holder.findViewById(R.id.bootanimation_preview_image);
+        mLoadingSpinner = (ProgressBar) holder.findViewById(R.id.bootanimation_loading_spinner);
+        loadBootAnimationPreview();
+    }
+
+    public void loadBootAnimationPreview() {
+        if (mCurrentTask != null && mCurrentTask.getStatus() != AsyncTask.Status.FINISHED) {
+            mCurrentTask.cancel(true);
+        }
+        int bootAnimStyle = BootAnimationUtils.getBootAnimStyle();
+        if (bootAnimStyle == 2 || bootAnimStyle == 3) {
+            if (mImageView != null) {
+                Drawable drawable = getContext().getDrawable(
+                        bootAnimStyle == 2 ? R.drawable.google_gemini : R.drawable.google_monet);
+                mImageView.setImageDrawable(drawable);
+            }
+        } else {
+            mCurrentTask = new LoadPreviewTask();
+            mCurrentTask.execute();
+        }
+    }
+
+    private class LoadPreviewTask extends AsyncTask<Void, Void, AnimationDrawable> {
+        @Override
+        protected AnimationDrawable doInBackground(Void... voids) {
+            if (isCancelled()) return null;
+            AnimationDrawable originalDrawable = BootAnimationUtils.getBootAnimationFrames(getContext());
+            if (originalDrawable == null) {
+                return null;
+            }
+            AnimationDrawable fixedDrawable = new AnimationDrawable();
+            for (int i = 0; i < originalDrawable.getNumberOfFrames(); i++) {
+                if (isCancelled()) return null;
+                Drawable frame = originalDrawable.getFrame(i);
+                int duration = originalDrawable.getDuration(i);
+                if (duration < 16) { // 16 ms is around 60fps
+                    duration = 1000 / 60; // Set to 60fps as a fallback
+                }
+                fixedDrawable.addFrame(frame, duration);
+            }
+            fixedDrawable.setOneShot(false); // Ensure the animation loops
+            return fixedDrawable;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mImageView != null) {
+                mImageView.setVisibility(View.GONE);
+            }
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(AnimationDrawable animationDrawable) {
+            if (isCancelled()) return;
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.GONE);
+            }
+            if (mImageView != null) {
+                mImageView.setVisibility(View.VISIBLE);
+            }
+            if (animationDrawable != null) {
+                mImageView.setImageDrawable(animationDrawable);
+                animationDrawable.start();
+            }
+        }
+    }
+}

src/com/everest/basecamp/fragments/BootAnimationSettings.java
@@ -1,0 +1,172 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemProperties;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.documentfile.provider.DocumentFile;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+import com.everest.basecamp.preferences.BootAnimationPreviewPreference;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class BootAnimationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String BOOTANIMATION_STYLE_KEY = "persist.sys.bootanimation_style";
+    private static final String TAG = "BootAnimationSettings";
+    private static final int REQUEST_CODE_PICK_ZIP = 1001;
+    private static final String CUSTOM_BOOTANIMATION_FILE = "/data/misc/bootanim/bootanimation.zip";
+
+    private ListPreference mBootAnimationStyle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.everest_bootanimation);
+
+        mBootAnimationStyle = (ListPreference) findPreference(BOOTANIMATION_STYLE_KEY);
+        if (mBootAnimationStyle != null) {
+            mBootAnimationStyle.setOnPreferenceChangeListener(this);
+
+            // Set the current value from the system property
+            int currentStyle = SystemProperties.getInt(BOOTANIMATION_STYLE_KEY, 0);
+            mBootAnimationStyle.setValue(String.valueOf(currentStyle));
+            updateBootAnimationPreview();
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBootAnimationStyle) {
+            int style = Integer.parseInt((String) newValue);
+            if (style == 5) { // Custom option selected
+                launchFilePicker();
+                return false; // Return false to prevent immediate property update
+            } else {
+                SystemProperties.set(BOOTANIMATION_STYLE_KEY, (String) newValue);
+                updateBootAnimationPreview();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void launchFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/zip");
+        startActivityForResult(intent, REQUEST_CODE_PICK_ZIP);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICK_ZIP && resultCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                handleSelectedFile(uri);
+            }
+        }
+    }
+
+    private void handleSelectedFile(Uri uri) {
+        try {
+            // Copy the selected file to the custom bootanimation location
+            InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
+            File customBootAnimation = new File(CUSTOM_BOOTANIMATION_FILE);
+            // Ensure the directory exists
+            customBootAnimation.getParentFile().mkdirs();
+            try (OutputStream outputStream = new FileOutputStream(customBootAnimation)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            }
+            inputStream.close();
+            // Update system property to use custom bootanimation
+            SystemProperties.set(BOOTANIMATION_STYLE_KEY, "5"); // Custom option value
+            updateBootAnimationPreview();
+            // Force the preference to update to the custom option
+            mBootAnimationStyle.setValue("5"); // Set to the custom option
+        } catch (Exception e) {
+            Log.e(TAG, "Error copying custom bootanimation", e);
+        }
+    }
+
+    private void updateBootAnimationPreview() {
+        BootAnimationPreviewPreference previewPreference = (BootAnimationPreviewPreference) findPreference("bootanimation_preview");
+        if (previewPreference != null) {
+            previewPreference.loadBootAnimationPreview();
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVEREST;
+    }
+
+    /**
+     * For search
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.everest_bootanimation;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}

Project-Mist-OS/packages_apps_Mistify

src/com/android/settings/preferences/BootAnimationPreviewPreference.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.preferences;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.android.settings.R;
+import com.android.settings.utils.BootAnimationUtils;
+
+public class BootAnimationPreviewPreference extends Preference {
+
+    private ImageView mImageView;
+    private ProgressBar mLoadingSpinner;
+    private LoadPreviewTask mCurrentTask;
+
+    public BootAnimationPreviewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_bootanimation_preview);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mImageView = (ImageView) holder.findViewById(R.id.bootanimation_preview_image);
+        mLoadingSpinner = (ProgressBar) holder.findViewById(R.id.bootanimation_loading_spinner);
+        loadBootAnimationPreview();
+    }
+
+    public void loadBootAnimationPreview() {
+        if (mCurrentTask != null && mCurrentTask.getStatus() != AsyncTask.Status.FINISHED) {
+            mCurrentTask.cancel(true);
+        }
+        int bootAnimStyle = BootAnimationUtils.getBootAnimStyle();
+        if (bootAnimStyle == 2 || bootAnimStyle == 3) {
+            if (mImageView != null) {
+                Drawable drawable = getContext().getDrawable(
+                        bootAnimStyle == 2 ? R.drawable.google_gemini : R.drawable.google_monet);
+                mImageView.setImageDrawable(drawable);
+            }
+        } else {
+            mCurrentTask = new LoadPreviewTask();
+            mCurrentTask.execute();
+        }
+    }
+
+    private class LoadPreviewTask extends AsyncTask<Void, Void, AnimationDrawable> {
+        @Override
+        protected AnimationDrawable doInBackground(Void... voids) {
+            if (isCancelled()) return null;
+            AnimationDrawable originalDrawable = BootAnimationUtils.getBootAnimationFrames(getContext());
+            if (originalDrawable == null) {
+                return null;
+            }
+            AnimationDrawable fixedDrawable = new AnimationDrawable();
+            for (int i = 0; i < originalDrawable.getNumberOfFrames(); i++) {
+                if (isCancelled()) return null;
+                Drawable frame = originalDrawable.getFrame(i);
+                int duration = originalDrawable.getDuration(i);
+                if (duration < 16) { // 16 ms is around 60fps
+                    duration = 1000 / 60; // Set to 60fps as a fallback
+                }
+                fixedDrawable.addFrame(frame, duration);
+            }
+            fixedDrawable.setOneShot(false); // Ensure the animation loops
+            return fixedDrawable;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mImageView != null) {
+                mImageView.setVisibility(View.GONE);
+            }
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(AnimationDrawable animationDrawable) {
+            if (isCancelled()) return;
+            if (mLoadingSpinner != null) {
+                mLoadingSpinner.setVisibility(View.GONE);
+            }
+            if (mImageView != null) {
+                mImageView.setVisibility(View.VISIBLE);
+            }
+            if (animationDrawable != null) {
+                mImageView.setImageDrawable(animationDrawable);
+                animationDrawable.start();
+            }
+        }
+    }
+}

src/com/mist/mistify/fragments/BootAnimationSettings.java
@@ -1,0 +1,172 @@
+/*
+ * Copyright (C) 2024 risingOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mist.mistify.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemProperties;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.documentfile.provider.DocumentFile;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.preferences.BootAnimationPreviewPreference;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class BootAnimationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String BOOTANIMATION_STYLE_KEY = "persist.sys.bootanimation_style";
+    private static final String TAG = "BootAnimationSettings";
+    private static final int REQUEST_CODE_PICK_ZIP = 1001;
+    private static final String CUSTOM_BOOTANIMATION_FILE = "/data/misc/bootanim/bootanimation.zip";
+
+    private ListPreference mBootAnimationStyle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.mist_bootanimation);
+
+        mBootAnimationStyle = (ListPreference) findPreference(BOOTANIMATION_STYLE_KEY);
+        if (mBootAnimationStyle != null) {
+            mBootAnimationStyle.setOnPreferenceChangeListener(this);
+
+            // Set the current value from the system property
+            int currentStyle = SystemProperties.getInt(BOOTANIMATION_STYLE_KEY, 0);
+            mBootAnimationStyle.setValue(String.valueOf(currentStyle));
+            updateBootAnimationPreview();
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBootAnimationStyle) {
+            int style = Integer.parseInt((String) newValue);
+            if (style == 5) { // Custom option selected
+                launchFilePicker();
+                return false; // Return false to prevent immediate property update
+            } else {
+                SystemProperties.set(BOOTANIMATION_STYLE_KEY, (String) newValue);
+                updateBootAnimationPreview();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void launchFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/zip");
+        startActivityForResult(intent, REQUEST_CODE_PICK_ZIP);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICK_ZIP && resultCode == Activity.RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                handleSelectedFile(uri);
+            }
+        }
+    }
+
+    private void handleSelectedFile(Uri uri) {
+        try {
+            // Copy the selected file to the custom bootanimation location
+            InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
+            File customBootAnimation = new File(CUSTOM_BOOTANIMATION_FILE);
+            // Ensure the directory exists
+            customBootAnimation.getParentFile().mkdirs();
+            try (OutputStream outputStream = new FileOutputStream(customBootAnimation)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            }
+            inputStream.close();
+            // Update system property to use custom bootanimation
+            SystemProperties.set(BOOTANIMATION_STYLE_KEY, "5"); // Custom option value
+            updateBootAnimationPreview();
+            // Force the preference to update to the custom option
+            mBootAnimationStyle.setValue("5"); // Set to the custom option
+        } catch (Exception e) {
+            Log.e(TAG, "Error copying custom bootanimation", e);
+        }
+    }
+
+    private void updateBootAnimationPreview() {
+        BootAnimationPreviewPreference previewPreference = (BootAnimationPreviewPreference) findPreference("bootanimation_preview");
+        if (previewPreference != null) {
+            previewPreference.loadBootAnimationPreview();
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.MIST;
+    }
+
+    /**
+     * For search
+     */
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.mist_bootanimation;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}