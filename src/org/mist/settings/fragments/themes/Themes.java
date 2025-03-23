1d8d8f1aa17df8e1a2a369a91392583e4f393fcc
AnierinB <anierin@evolution-x.org>
2025-03-23 17:12:20 +0530

    Mistify: Support navbar themes in both Launcher3 & NexusLauncher
    * Also remove retired navbar features
    
    Revert "Mistify: Add separate tunables for navbar layout customization [2/2]"
    
    This reverts commit 575212bf58c30e75540e109526a63337130df73b.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/mist/settings/fragments/themes/Themes.java
+++ src/org/mist/settings/fragments/themes/Themes.java
@@ -42,7 +42,6 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_LOCK_SOUND = "lock_sound";
     private static final String KEY_UNLOCK_SOUND = "unlock_sound";
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
-    private static final String KEY_NAVBAR_ICON = "android.theme.customization.navbar";
     private static final String KEY_SIGNAL_ICON = "android.theme.customization.signal_icon";
     private static final String KEY_UDFPS_ICON = "udfps_icon";
     private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
@@ -78,7 +77,6 @@ public class Themes extends SettingsPreferenceFragment implements
     private GlobalSettingListPreference mUnlockSound;
     private PreferenceCategory mLauncherCategory;
     private PreferenceCategory mIconsCategory;
-    private Preference mNavbarIcon;
     private Preference mSignalIcon;
     private Preference mUdfpsIcon;
     private PreferenceCategory mAnimationsCategory;
@@ -105,7 +103,6 @@ public class Themes extends SettingsPreferenceFragment implements
         mUnlockSound.setOnPreferenceChangeListener(this);
         mLauncherCategory = (PreferenceCategory) findPreference(KEY_LAUNCHER_CATEGORY);
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
-        mNavbarIcon = (Preference) findPreference(KEY_NAVBAR_ICON);
         mSignalIcon = (Preference) findPreference(KEY_SIGNAL_ICON);
         mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
         mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
@@ -113,10 +110,6 @@ public class Themes extends SettingsPreferenceFragment implements
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mSignalIcon);
-        }
-
-        if (DeviceUtils.isEdgeToEdgeEnabled(context)) {
-            mIconsCategory.removePreference(mNavbarIcon);
         }
 
         FingerprintManager fingerprintManager = (FingerprintManager)
