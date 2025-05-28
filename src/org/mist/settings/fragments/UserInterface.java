f7333225aa5f65b07b5ec14aaa3276a32b299e68
ShukakuZa <yusufzuzu787@gmail.com>
2025-05-28 20:22:26 +0000

    Bring Up Dashboard Styles

---
+++ src/org/mist/settings/fragments/UserInterface.java
@@ -0,0 +1,104 @@
+
+/*
+ * Copyright (C) 2016-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mist.settings.fragments;
+
+import static com.android.settings.mist.MistConstants.DEFAULT_DASHBOARD_STYLE;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.mist.SystemRestartUtils;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
+
+@SearchIndexable
+public class UserInterface extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener{
+
+    public static final String TAG = "UserInterface";
+
+    private static final String KEY_DASHBOARD_STYLE = "settings_dashboard_style";
+
+    private ListPreference mDashBoardStyle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.mist_settings_ui);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        ListPreference mDashBoardStyle =
+                prefScreen.findPreference(KEY_DASHBOARD_STYLE);
+        mDashBoardStyle.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (KEY_DASHBOARD_STYLE.equals(preference.getKey())) {
+            SystemRestartUtils.showSettingsRestartDialog(getContext());
+            return true;
+        }
+        return false;
+    }
+
+    public static void reset(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SETTINGS_DASHBOARD_STYLE,
+                DEFAULT_DASHBOARD_STYLE, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.MIST;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.mist_settings_ui) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}
